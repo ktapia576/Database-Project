0c9dcb2d75ff4b9ff34c75de486e3fc59ade60be
ktapia576 <ktapia576@hotmail.com>
2019-04-15 12:24:33 -0400

    Chaneg file name and add functions

---
+++ utility.js
@@ -0,0 +1,8 @@
+function logout() {
+    document.cookie = "customerID=; expires=Thu, 18 Dec 1970 12:00:00 UTC; path=./" // Set Cookie to expire
+    window.location.href = "p2.html" // redirect user to p2.html
+}
+
+function addTransaction() {
+    
+}
