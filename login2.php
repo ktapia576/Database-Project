ce2fabe60fb2ef2d203821c8fe3523d04fb0f5a5
ktapia576 <ktapia576@hotmail.com>
2019-04-15 00:23:43 -0400

    Fix css path

--- login2.php
+++ login2.php
@@ -3,7 +3,7 @@
     <head>
         <title>CPS3740 Project</title>
 
-        <link href="styles/style.css" rel="stylesheet" type="text/css">
+        <link href="style.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
         <script defer src="logout.js"></script>
