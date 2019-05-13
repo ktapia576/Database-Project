aeca62f21cfec67d11bf7ffe61d2a353ef08fc3e
ktapia576 <ktapia576@hotmail.com>
2019-05-13 19:32:47 -0400

    Fixing php syntax from crashing

--- display_update_transaction.php
+++ display_update_transaction.php
@@ -3,7 +3,6 @@
     <head>
         <title>CPS3740 Project</title>
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link href="styles.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 
@@ -56,7 +55,7 @@
                     printf("<b>".$name."</b> current balance is <b>NULL</b>. May not have any transactional records");
                 }
                 else {
-                    printf("<b>".$name."</b> current balance is <b>%.2f</b>.",$balance);
+                    printf("<b>".$name."</b> current balance is <b>%.2f</b>.<br>",$balance);
                 }
 
                 // --------------------------------- Create table --------------------------------
@@ -67,8 +66,9 @@
 
                 if($result->num_rows > 0) {   // Check if there are records found
                     // Print Header columns of table
-                    echo "<form method='POST' action='insert_transaction.php'>";
-                    echo "<table class='highlight' style='width: 50%;'><tr><th>ID</th><th>Code</th><th>Operation</th><th>Amount</th><th>Date Time</th><th>Note</th><th>Delete</th></tr>";
+                    echo "<form method='POST' action='update_transaction.php'>";
+                    print("<table class='highlight' style='width: 50%;'><tr><th>ID</th><th>Code</th>
+                            <th>Operation</th><th>Amount</th><th>Date Time</th><th>Note</th><th>Delete</th></tr>");
 
                     // Print rows with data
                     while($row = $result->fetch_assoc()) {
@@ -80,7 +80,7 @@
                         }
                         print("<tr><td>".$row["mid"]."</td><td>".$row["code"]."</td>".$type.
                         $row["amount"]."</td><td>".$row["mydatetime"]."</td><td style='background-color: yellow;'>".$row["note"]."</td>
-                        <td><input type="checkbox" name="transCode[]" value=".$row["code"]."></td></tr>"); 
+                        <td><input type='checkbox' name='transCode[]' value='".$row["code"]."'></td></tr>"); 
                     }
 
                     echo"</table><br>";
--- update_transaction.php
+++ update_transaction.php
@@ -14,109 +14,17 @@
     <body>
         <?php
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                // Check if amount empty
-                if(empty($_POST["transaction-amount"])){
-                    die("You must enter an amount!");
-                }
-
-                if(empty($_POST["transaction-source"])){
-                    die("You must select a Source!");
-                }
-
-                if(empty($_POST["transaction-type"])){
-                    die("You must select a transaction type!");
-                }
-
-                $amount=$_POST["transaction-amount"];
-                $type=$_POST["transaction-type"];
-                $code=$_POST["transaction-code"];
-                $source=$_POST["transaction-source"];
-                $note=null;
-
-                // Check if note was entered
-                if(!empty($_POST["transaction-note"])){
-                    $note=$_POST["transaction-note"];
-                }
-
-                // Check if number
-                if(!is_numeric($amount)){   // TRUE if var_name is a number or a numeric string, FALSE otherwise.
-                   die("You can only enter numerical values for amount!");
-                }
+                $transCodes = $_POST['transCode'];
                 
-                if($amount <= 0){
-                    die("You have entered the amount: ".$amount." | That is less than 0.");
+                $count = count($transCodes);
+
+                for($i=0; $i<$count;$i++){
+                    echo $transCodes[$i] . " ";
                 }
 
-                // ------------------- Connect to Database ---------------------------
-                function db_connect() {
-                    $conn;
-                    
-                    if(!isset($conn)) {
-                        $config = parse_ini_file('config.ini'); // Load database config
-                        $conn = new mysqli($config['servername'],$config['username'],$config['password'],$config['dbname']);    // Create connection to database using config.ini
-                    }
-
-                    if($conn === false) {
-                        return mysqli_connect_error(); 
-                    }
-                    return $conn;
-                }
-
-                // Connect to the database
-                $conn = db_connect();
-
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                }
-                // --------------------------------------------------------------------
-
-                $id = $_COOKIE['customerID'];
-
-                $sql = "SELECT SUM(amount) as balance FROM CPS3740_2019S.Money_tapiake WHERE cid='$id'";    // Get balance of User based on ID
-
-                // Get result or show error and die
-                $result = $conn->query($sql) or die($conn->error);
-                $row = $result->fetch_assoc();
-                
-                $balance=$row['balance'];
-
-                // ------------ Check if duplicate code entry ---------------
-                $sql = "SELECT * FROM CPS3740_2019S.Money_tapiake WHERE code='$code'";
-
-                $result = $conn->query($sql) or die($conn->error);
-
-                if ($result->num_rows == 1) {
-                    die("There is a duplicate entry for the transaction code you have entered!");
-                }
-                // ----------------------------------------------------------
-
-                if($type == "W"){
-                    if($amount <= $balance){  // Check balance before withdrawal  
-                        $amount="-".$amount;
-                        $sql="INSERT INTO CPS3740_2019S.Money_tapiake (code, cid, type, amount, mydatetime, note, sid)
-                        VALUES ('$code', '$id', '$type', '$amount', NOW(), '$note', '$source')";
-
-                        // Get result or show error and die
-                        $result = $conn->query($sql) or die($conn->error);
-
-                        print("<h1>Your Withdrawal of <b>".$amount."</b> was successful!");
-                    }
-                    else{
-                        die("Your balance is lower than the withrawal amount!");
-                    }
-                }
-                elseif($type == "D"){
-                    $sql="INSERT INTO CPS3740_2019S.Money_tapiake (code, cid, type, amount, mydatetime, note, sid)
-                    VALUES ('$code', '$id', '$type', '$amount', NOW(), '$note', '$source')";
-                    
-                    // Get result or show error and die
-                    $result = $conn->query($sql) or die($conn->error);
-
-                    print("<p>Your Deposit of <b>".$amount."</b> was successful!</p>");
-                }
             }
             else {
-                die("<p>Must use add transaction form!</p>");
+                die("<p>Must use update transaction form!</p>");
             }
         ?>
     </body>
